628d3e5eaf4b5d2b12f58379ef61ca8872ba503b
Nepochatov <sergii.nepochatov@takeoff.com>
2021-07-21 13:13:25 +0300

    stop creating an issue

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -9,12 +9,12 @@ async function run() {
 
         const octokit = new github.getOctokit(token);
 
-        const newIssue = await octokit.issues.create({
-            repo: github.context.repo.repo,
-            owner: github.context.repo.owner,
-            title: issueTitle,
-            body: jokeBody
-        });
+        // const newIssue = await octokit.issues.create({
+        //     repo: github.context.repo.repo,
+        //     owner: github.context.repo.owner,
+        //     title: issueTitle,
+        //     body: jokeBody
+        // });
     } catch (error) {
         core.setFailed(error.message);
     }
